8489d4a1d9da9c1b6f7b0b8eeda4bf48c5d76af5
James Bungard <jmbungard@gmail.com>
2018-11-05 21:29:51 +1100

    Add bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import Vue from "vue";
+
+// Bootstrap
+import BootstrapVue from "bootstrap-vue"
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Custom
 import HelloComponent from "./components/Hello.vue";
 
 let v = new Vue({
     el: "#app",
     template: `
     <div>
+        <b-alert show>Default alert</b-alert>
         Name: <input v-model="name" type="text">
         <hello-component :name="name" :initialEnthusiasm="5" />
     </div>
